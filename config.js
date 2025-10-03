d3cd0ac1637d6791b75e63e7fdc6dafce7aa0e5a
Carlos Eduardo Nicodemo <95039221+CaduNicodemo@users.noreply.github.com>
2025-10-03 08:40:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -18,6 +18,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
 
