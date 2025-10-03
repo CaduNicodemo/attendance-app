e62fd0cdd042b739967d45896fb704f9fc881460
Carlos Eduardo Nicodemo <95039221+CaduNicodemo@users.noreply.github.com>
2025-10-03 08:27:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-/* ---------------- Firestore Setup ---------------- */
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, doc, setDoc, getDocs, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAm4kct4BJrVydJpcAXFZuHuEOAtnpUJfc",
-  authDomain: "attendance-rb.firebaseapp.com",
-  projectId: "attendance-rb",
-  storageBucket: "attendance-rb.firebasestorage.app",
-  messagingSenderId: "884478984062",
-  appId: "1:884478984062:web:e64f5dedafb34ac7d73d2f"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
 /* ---------------- Estado de autenticação ---------------- */
 onAuthStateChanged(auth, (user) => {
   // Alguns elementos podem não existir se o HTML mudar; guardamos com checks
