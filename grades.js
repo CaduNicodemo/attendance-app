89ec982e36f2e2fbd6387a4b91a4dd3ba5cfb56d
Carlos Eduardo Nicodemo <95039221+CaduNicodemo@users.noreply.github.com>
2025-10-03 08:28:51 -0300

    Update grades.js

--- grades.js
+++ grades.js
@@ -1,20 +1,4 @@
-/* ---------------- Firestore Setup ---------------- */
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAm4kct4BJrVydJpcAXFZuHuEOAtnpUJfc",
-  authDomain: "attendance-rb.firebaseapp.com",
-  projectId: "attendance-rb",
-  storageBucket: "attendance-rb.firebasestorage.app",
-  messagingSenderId: "884478984062",
-  appId: "1:884478984062:web:e64f5dedafb34ac7d73d2f"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+import { auth, db } from "./config.js";
 
 let currentGroupName = localStorage.getItem("currentGroup");
 let currentGroupData = null;
