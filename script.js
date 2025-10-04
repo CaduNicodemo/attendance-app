f43bb09113e91908ada2328bd51d4839f9be5033
Carlos Eduardo Nicodemo <95039221+CaduNicodemo@users.noreply.github.com>
2025-10-04 10:35:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-// Firebase Auth
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } 
-  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-// Firebase Firestore
-import { getFirestore, collection, doc, setDoc, getDoc, getDocs, updateDoc, deleteDoc } 
-  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-// Importa auth e db do config.js (já inicializados)
-import { auth, db } from "./config.js";
+// Importa do config.js
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut, getFirestore, collection, doc, setDoc, getDoc, getDocs, updateDoc, deleteDoc, auth, db } from "./config.js";
 
 
 /* ---------------- Estado de autenticação ---------------- */
